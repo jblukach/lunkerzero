743b7a25caa05d8808d993108a147805853a0425
John Lukach <john@lukach.net>
2024-06-30 20:59:53 +0000

    environments

--- lunker/development/lunker.py
+++ lunker/development/lunker.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import ipaddress
 import os
+import requests
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
@@ -51,130 +52,12 @@ def handler(event, context):
 
     print(event) ### DEBUG ###
 
-    try:
+    code = 200
+    msg = 'Hello from Lunker!'
 
-        keys = event.keys()
 
-        for key in keys:
 
-            if key.lower() == 'add':
 
-                count = primarykey('IOCS#')
-                if len(count) >= int(os.environ['LUNKER_LIMIT']):
-                    code = 403
-                    msg = 'Maximum Quota Reached'
-                else:
-                    code = 403
-                    msg = 'Invalid '+str(event['add'])
-                    try:
-                        if ipaddress.ip_network(event['add']).version == 4:
-                            table.put_item(
-                                Item = {
-                                    'pk': 'IOCS#',
-                                    'sk': 'IOCS#IPV4#'+str(event['add']),
-                                    'ioc': str(event['add'])
-                                }
-                            )
-                            code = 200
-                            msg = 'Added '+str(event['add'])
-                    except:
-                        pass
-                    try:
-                        if ipaddress.ip_network(event['add']).version == 6:
-                            table.put_item(
-                                Item = {
-                                    'pk': 'IOCS#',
-                                    'sk': 'IOCS#IPV6#'+str(event['add']),
-                                    'ioc': str(event['add'])
-                                }
-                            )
-                            code = 200
-                            msg = 'Added '+str(event['add'])
-                    except:
-                        pass
-                    try:
-                        tlds = tldlist('TLD#')
-                        tlds = [x['sk'] for x in tlds]
-                        if event['add'].lower().split('.')[-1] in tlds:
-                            if event['add'].lower().startswith('http://'):
-                                event['add'] = event['add'][7:]
-                            if event['add'].lower().startswith('https://'):
-                                event['add'] = event['add'][8:]
-                            table.put_item(
-                                Item = {
-                                    'pk': 'IOCS#',
-                                    'sk': 'IOCS#DNS#'+str(event['add'].lower()),
-                                    'ioc': str(event['add'].lower())
-                                }
-                            )
-                            code = 200
-                            msg = 'Added '+str(event['add'])
-                    except:
-                        pass
-
-            elif key.lower() == 'list':
-
-                code = 200
-                if event['list'].lower() == 'dns':
-                    msg = sortkey('IOCS#', 'IOCS#DNS#')
-                    msg = [x['ioc'] for x in msg]
-                elif event['list'].lower() == 'ipv4':
-                    msg = sortkey('IOCS#', 'IOCS#IPV4#')
-                    msg = [x['ioc'] for x in msg]
-                elif event['list'].lower() == 'ipv6':
-                    msg = sortkey('IOCS#', 'IOCS#IPV6#')
-                    msg = [x['ioc'] for x in msg]
-                else:
-                    msg = primarykey('IOCS#')
-                    msg = [x['ioc'] for x in msg]
-
-            elif key.lower() == 'remove':
-
-                code = 403
-                msg = 'Failure '+str(event['remove'])
-                try:
-                    if ipaddress.ip_network(event['remove']).version == 4:
-                        table.delete_item(
-                            Key = {
-                                'pk': 'IOCS#',
-                                'sk': 'IOCS#IPV4#'+str(event['remove'])
-                            }
-                        )
-                        msg = 'Removed '+str(event['remove'])
-                except:
-                    pass
-                try:
-                    if ipaddress.ip_network(event['remove']).version == 6:
-                        table.delete_item(
-                            Key = {
-                                'pk': 'IOCS#',
-                                'sk': 'IOCS#IPV6#'+str(event['remove'])
-                            }
-                        )
-                        msg = 'Removed '+str(event['remove'])
-                except:
-                    pass
-                try:
-                    tlds = tldlist('TLD#')
-                    tlds = [x['sk'] for x in tlds]
-                    if event['remove'].lower().split('.')[-1] in tlds:
-                        if event['remove'].lower().startswith('http://'):
-                            event['remove'] = event['remove'][7:]
-                        if event['remove'].lower().startswith('https://'):
-                            event['remove'] = event['remove'][8:]
-                        table.delete_item(
-                            Key = {
-                                'pk': 'IOCS#',
-                                'sk': 'IOCS#DNS#'+str(event['remove']).lower()
-                            }
-                        )
-                        msg = 'Removed '+str(event['remove'])
-                except:
-                    pass
-
-    except Exception as e:
-        msg = 'Where the Internet Ends'
-        code = 404
 
     return {
         'statusCode': code,
